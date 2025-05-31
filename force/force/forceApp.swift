30cc4741b02e176e620e83b28a49c73244b30b2a
matt-fang <matthewfang.calif@gmail.com>
2025-05-31 15:42:31 -0700

    app setup

---
+++ force/force/forceApp.swift
@@ -0,0 +1,17 @@
+//
+//  forceApp.swift
+//  force
+//
+//  Created by Matthew Fang on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct forceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ force/forceTests/forceTests.swift
@@ -0,0 +1,17 @@
+//
+//  forceTests.swift
+//  forceTests
+//
+//  Created by Matthew Fang on 5/31/25.
+//
+
+import Testing
+@testable import force
+
+struct forceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
